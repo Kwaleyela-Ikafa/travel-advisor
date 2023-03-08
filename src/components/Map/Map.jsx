b9c9ef03fafb97a23ba45c911b6fba06849dbc65
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-03-08 13:29:41 +0200

    Update map component

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React from 'react';
+import GoogleMapReact from 'google-map-react';
+// import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+// import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+// import Rating from '@material-ui/lab/Rating';
 
-const List = () => {
-    return(
-        <h1>Map</h1>
-    )
-}
+// import mapStyles from '../../mapStyles';
+import useStyles from './styles.js';
 
-export default List;
+const Map = () => {
+//   const isMobile = useMediaQuery('(min-width:600px)');
+  const classes = useStyles();
+  const coordinates = { lat: 0, lng: 0 }
+
+  return (
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: 'AIzaSyDpZVdmoBXY_luIWVa5-bOTi0aZUUbbr2o' }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+        options={''}
+        onChange={''}
+        onChildClick={''}
+      >
+
+      </GoogleMapReact>
+    </div>
+  );
+};
+
+export default Map;
