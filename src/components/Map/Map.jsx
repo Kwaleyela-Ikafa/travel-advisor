dc962fa13a9d27cc58cd04ec19576dd228af6a67
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-03-08 19:36:00 +0200

    Comment faulty code

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,28 +1,31 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
-// import { Paper, Typography, useMediaQuery } from '@material-ui/core';
-// import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-// import Rating from '@material-ui/lab/Rating';
+import { Paper, Typography, useMediaQuery } from '@material-ui/core';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
 
 // import mapStyles from '../../mapStyles';
 import useStyles from './styles.js';
 
 const Map = () => {
-//   const isMobile = useMediaQuery('(min-width:600px)');
   const classes = useStyles();
-  const coordinates = { lat: 0, lng: 0 }
+  const isMobile = useMediaQuery('(min-width:600px)');
+
+  const coordinates = { lat: 0, lng: 0 };
 
   return (
     <div className={classes.mapContainer}>
+        <h1>Map</h1>
+
       <GoogleMapReact
-        bootstrapURLKeys={{ key: '' }}
-        defaultCenter={coordinates}
-        center={coordinates}
-        defaultZoom={14}
-        margin={[50, 50, 50, 50]}
-        options={''}
-        onChange={''}
-        onChildClick={''}
+        // bootstrapURLKeys={{ key: '' }}
+        // defaultCenter={coordinates}
+        // center={coordinates}
+        // defaultZoom={14}
+        // margin={[50, 50, 50, 50]}
+        // options={''}
+        // onChange={''}
+        // onChildClick={''}
       >
 
       </GoogleMapReact>
