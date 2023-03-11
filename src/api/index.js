25b6c615187cf91335d807535b809e2efced5c9f
kwaleyela-ikafa <kwaleyelamusil@gmail.com>
2023-03-11 14:50:42 +0200

    update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,22 +1,9 @@
 import axios from "axios";
-const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
+// const URL = 'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
 
-// const options = {
-//     params: {
-//       bl_latitude: '11.847676',
-//       tr_latitude: '12.838442',
-//       bl_longitude: '109.095887',
-//       tr_longitude: '109.149359',
-//     },
-//     headers: {
-//       'X-RapidAPI-Key': '27ed715f6bmshc8c99823f0e51fbp1fa633jsndc1087e52af8',
-//       'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-//     }
-//   }; 
-
-export const getPlacesData = async (sw, ne) => {
+export const getPlacesData = async (type, sw, ne) => {
   try {
-    const { data: { data } } = await axios.get(URL, {
+    const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
       params: {
         bl_latitude: sw.lat,
         tr_latitude: ne.lat,
